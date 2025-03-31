fb79643cd2bc2bbcd675083a4a51f2395f00ce6e
sudown <mirosmarsantoss@gmail.com>
2025-03-31 08:46:06 -0300

    back: configuracao do projeto

---
+++ backend/Infra/Database/Sqlite/EntityFramework/EntitiesConfiguration/BaseEntityConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Database.Sqlite.EntityFramework.EntitiesConfiguration
+{
+    public class BaseEntityConfiguration : IEntityTypeConfiguration<BaseEntity>
+    {
+        public void Configure(EntityTypeBuilder<BaseEntity> builder)
+        {
+            builder.HasKey(be => be.Id);
+
+            builder.Property(be => be.CreatedAt)
+                .IsRequired();
+
+            builder.Property(be => be.UpdatedAt)
+                .IsRequired(false);
+
+            builder.Property(be => be.DeletedAt)
+                .IsRequired(false);
+
+            builder.Property(be => be.IsDeleted)
+                .IsRequired();
+        }
+    }
+}
---
+++ backend/Infra/Gateways/GenericgatewayRepository.cs
@@ -0,0 +1,46 @@
+﻿using Application.Gateways;
+using Domain.Entites;
+using Infra.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Gateways
+{
+    public class GenericgatewayRepository<T>(PolifarmDbContext context) : IGenericGateway<T> where T : BaseEntity
+    {
+        public async Task AddAsync(T entity)
+        {
+            await context.AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await context.Set<T>().FindAsync(id);
+            context.Remove(entity!);
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            var entities = await context.Set<T>().ToListAsync();
+            return entities;
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id)
+        {
+            var entity = await context.Set<T>().FindAsync(id);
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            entity.UpdatedAt = DateTime.Now;
+            context.Update(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
