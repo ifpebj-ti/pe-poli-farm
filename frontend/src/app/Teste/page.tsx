ad06a5baa48699fc6f3815ccd4d6b207c701e8d0
Fátima Cristina <fatimacristina1906@gmail.com>
2025-06-01 20:41:25 -0300

    feat:Card Agendamento

--- frontend/src/app/Teste/page.tsx
+++ frontend/src/app/Teste/page.tsx
@@ -1,3 +1,4 @@
+import AgendamentoCard from "@/src/components/Cardagendamento";
 import MenuInicio from "@/src/components/MenuInicio";
 import NavBar from "@/src/components/NavBar";
 import React from "react";
@@ -6,6 +7,7 @@ export default function Teste() {
   return (<>
     <NavBar />
     <MenuInicio />
+    <AgendamentoCard />
   </>
   );
 }
