96279d686eaacf20ea3036c9bb88b6c42bf3de09
José Neto <jnan@discente.ifpe.edu.br>
2025-06-01 17:24:11 -0300

    To merge

--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -23,11 +23,36 @@ export async function NewPassword(
   password: string,
   acessCode: string,
 ) {
-  const response = await api.put(`/Auth`, {
-    email,
-    password,
-    acessCode,
-  });
-  return response;
+  // 1. Obter o token do localStorage
+  const token = localStorage.getItem('UserAuth');
 
+  // 2. Verificar se o token existe
+  if (!token) {
+    console.error('Token não encontrado no localStorage.');
+    // Você pode querer lançar um erro ou retornar null/false aqui,
+    // dependendo da sua lógica de tratamento de erro.
+    throw new Error('Token de autenticação não encontrado.');
+  }
+
+  // 3. Configurar os cabeçalhos da requisição
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+
+  try {
+    // 4. Realizar a requisição PUT com os cabeçalhos
+    const response = await api.put(`/Auth`, {
+      email,
+      password,
+      acessCode,
+    }, {
+      headers, // Adiciona os cabeçalhos à requisição
+    });
+
+    return response;
+  } catch (error) {
+    console.error('Erro ao atualizar a senha:', error);
+    // Trate o erro conforme a necessidade da sua aplicação
+    throw error; // Re-lança o erro para que quem chamou a função possa tratá-lo
+  }
 }
