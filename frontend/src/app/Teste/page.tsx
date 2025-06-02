e1297811cfe07a476f023a7c37e1447c6559c9f6
Fátima Cristina <fatimacristina1906@gmail.com>
2025-06-01 22:03:12 -0300

    feat:Meus dados

--- frontend/src/app/Teste/page.tsx
+++ frontend/src/app/Teste/page.tsx
@@ -1,13 +1,14 @@
 import AgendamentoCard from "@/src/components/Cardagendamento";
 import MenuInicio from "@/src/components/MenuInicio";
+import MeusDadosForm from "@/src/components/Meusdados/page";
 import NavBar from "@/src/components/NavBar";
+import { Box } from "@mui/material";
 import React from "react";
 
 export default function Teste() {
-  return (<>
+  return (<Box sx={{ backgroundColor: 'white', minHeight: '100vh' }}>
     <NavBar />
-    <MenuInicio />
-    <AgendamentoCard />
-  </>
+    <MeusDadosForm />
+  </Box >
   );
 }
