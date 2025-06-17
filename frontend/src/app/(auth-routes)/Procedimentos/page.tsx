107da6793eae285a192a54ce41cd50814a8a239f
Fátima Cristina <fatimacristina1906@gmail.com>
2025-06-16 21:27:11 -0300

    feat(popup): adicionar pop-up

---
+++ frontend/src/app/(auth-routes)/Procedimentos/page.tsx
@@ -0,0 +1,13 @@
+import NavBar from '@/src/components/NavBar';
+import RegistroProcedimentos from '@/src/components/RegistroProcedimentos';
+
+import { Box } from '@mui/material';
+
+export default function RegistroProcedimentosPage() {
+  return (
+    <Box>
+      <NavBar />
+      <RegistroProcedimentos />
+    </Box>
+  );
+}
